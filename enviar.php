cf56def4f705f074f183ead2ef2b0fd61360e23c
Juan David <84122096+juandtrujillo99@users.noreply.github.com>
2022-04-02 23:15:26 +0200

    Add files via upload

---
+++ enviar.php
@@ -0,0 +1,10 @@
+<?php 
+	$destino= "ospino3596@hotmail.com";
+	$nombre = $_post["nombre"];
+	$correo = $_post["correo"];
+	$telefono = $_post["telefono"];
+	$mensaje = $_post["mensaje"];
+	$contenido = "Nombre: " . $nombre . "\nCorreo: " . $correo . "\nTelefono: " . $telefono . "\nMensaje: " . $mensaje;
+	mail($destino,"Contacto", $contenido);
+	header("Location:gracias.html");
+?>
